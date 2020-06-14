0c7a605f9e3b953137dff8c05ba5efc51ff2f410
davesmall28 <dave@dsmall.co.uk>
2020-06-14 23:31:37 +0100

    Update solution .Net version to v.4.6.2 and slimline the build process.

--- Springboard365.Xrm.UnitTest.Core/NuGet/NuGetPush.bat
+++ Springboard365.Xrm.UnitTest.Core/NuGet/NuGetPush.bat
@@ -1,7 +1,7 @@
-SET packageVersion=1.1.0
+SET packageVersion=2.0.0
 
-NuGet.exe pack ../Springboard365.Xrm.UnitTest.Core.csproj -Build -symbols -Version %packageVersion%
+NuGet.exe pack ../Springboard365.Xrm.UnitTest.Core.csproj -Build -Symbols -Version %packageVersion% -Properties Configuration=Release
 
-NuGet.exe push Springboard365.Xrm.UnitTest.Core.%packageVersion%.nupkg
+NuGet.exe push Springboard365.Xrm.UnitTest.Core.%packageVersion%.nupkg -source "https://api.nuget.org/v3/index.json"
 
 pause
